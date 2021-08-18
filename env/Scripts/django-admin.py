c9efd84a475bf2192964e462022192918f81f7fb
jhpiedrahitao <jhpiedrahitao@unal.edu.co>
2021-08-17 19:37:08 -0500

    Django inventario users app created

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!c:\users\innovacióncad\documents\cursos\misiontic2022\ciclo 4\proyecto\gestiondeusuariosinventario\env\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
---
+++ usersInventarioApp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import inventoryUser
+
+admin.site.register(inventoryUser)
